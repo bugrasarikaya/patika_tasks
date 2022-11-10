8cf1202942675badcfd587a321dd9f7a5e578304
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-11-10 16:09:12 +0300

    Second task was completed.

---
+++ csharp_tasks/task_2/main.cs
@@ -0,0 +1,46 @@
+﻿namespace task_1 {
+    internal class main {
+        static void Main(string[] args) {
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+            bool isSuccess = true;
+            bool isCompleted = false;
+            if (isSuccess && isCompleted) Console.WriteLine("Perfect");
+            if (isSuccess || isCompleted) Console.WriteLine("Great");
+            if (isSuccess && !isCompleted) Console.WriteLine("Fine");
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1++;
+            Console.WriteLine(sayi1);
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);
+        }
+    }
+}
