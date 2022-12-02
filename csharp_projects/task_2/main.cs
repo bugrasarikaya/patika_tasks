a2c2a1b73282461887b017240f170b42306f8163
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-12-02 12:48:43 +0300

    Second task of new course, was completed.

---
+++ csharp_projects/task_2/main.cs
@@ -0,0 +1,27 @@
+﻿namespace task_2 {
+    internal class main {
+        static void Main(string[] args) {
+            bool exception;
+            do {
+                Console.Write("Enter a height: ");
+                string? input = Console.ReadLine();
+                exception = false;
+                try {
+                    if (string.IsNullOrEmpty(input)) throw new Exception();
+                    draw_triangle(int.Parse(input));
+                } catch (Exception) {
+                    Console.WriteLine("Invalid Input");
+                    exception = true;
+                }
+            } while (exception);
+        }
+        static void draw_triangle(int height) {
+            int width = 1;
+            while(height-- > 0) {
+                for (int i = 0; i < width; i++) Console.Write("*");
+                width++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
