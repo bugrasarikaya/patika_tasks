be42034243e8791facc399bbff802e50b1c7eaaa
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-12-02 08:53:39 +0300

    First task of new course, was completed.

---
+++ csharp_projects/task_1/main.cs
@@ -0,0 +1,27 @@
+﻿namespace task_1 {
+    internal class main {
+        static void Main(string[] args) {
+            bool exception;
+            do {
+                Console.Write("Enter a depth: ");
+                string? input = Console.ReadLine();
+                exception = false;
+                try {
+                    if (string.IsNullOrEmpty(input)) throw new Exception();
+                    else Console.WriteLine("Result: " + mean(int.Parse(input)));
+                } catch (Exception) {
+                    Console.WriteLine("Invalid Input");
+                    exception = true;
+                }
+            } while (exception);
+        }
+        static int fibonacci(int depth) {
+            return depth <= 1 ? depth : fibonacci(depth - 1) + fibonacci(depth - 2);
+        }
+        static double mean(int number) {
+            double total = 0;
+            for (int i = 1; i <= number; i++) total += fibonacci(i);
+            return total / number;
+        }
+    }
+}
