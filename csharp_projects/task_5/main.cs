5186d75c207e175d46e26c2cf5b74931249c5d60
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-12-03 03:49:53 +0300

    Fifth task of new course, was completed.

---
+++ csharp_projects/task_5/main.cs
@@ -0,0 +1,13 @@
+﻿namespace task_5 {
+    internal class main {
+        static void Main(string[] args) {
+            Console.Write("Enter an input: ");
+            string? input = Console.ReadLine();
+            if(!string.IsNullOrEmpty(input)){
+                foreach (string element in input.Split()) {
+                    Console.Write(element.Substring(1, element.Length - 1) + element.Substring(0, 1) + " ");
+                }
+            }
+        }
+    }
+}
