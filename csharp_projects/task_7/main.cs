54bffeb0612bf0c62a3cf43cb4d961a4220ce27b
bugra-sarikaya <bugra_sarikaya@windowslive.com>
2022-12-03 18:10:18 +0300

    Seventh task of new course, was completed.

---
+++ csharp_projects/task_7/main.cs
@@ -0,0 +1,29 @@
+﻿namespace task_7 {
+    internal class main {
+        static void Main(string[] args) {
+            bool exception;
+            string? input;
+            do {
+                Console.Write("Enter an input: ");
+                input = Console.ReadLine();
+                exception = false;
+                try {
+                    if (string.IsNullOrEmpty(input)) throw new Exception();
+                    else {
+                        List<int> list_number = new List<int>();
+                        foreach (string element in input.Split()) list_number.Add(int.Parse(element));
+                        int i;
+                        for (i = 1; i < list_number.Count(); i += 2) {
+                            if (list_number[i - 1] == list_number[i]) Console.Write(Math.Pow(list_number[i - 1] + list_number[i], 2) + " ");
+                            else Console.Write(list_number[i - 1] + list_number[i] + " ");
+                        }
+                        if (list_number.Count() % 2 != 0) Console.Write(list_number[i - 1]);
+                    }
+                } catch (Exception) {
+                    Console.WriteLine("Invalid Input");
+                    exception = true;
+                }
+            } while (exception);
+        }
+    }
+}
